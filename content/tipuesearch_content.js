3fcd168ca53b9b30b78dd0194baa0a3ce2dc2a0b
s40923137 <asd31385118@gmail.com>
2022-03-02 12:16:07 +0800

    test

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site  \n 111111111111', 'tags': '', 'url': 'About.html'}]};
